c3089d3613a4e80861ece552fb74a95e23ad2f8a
Christopher Boardman <89698445+GameMill@users.noreply.github.com>
2025-09-02 23:52:51 +0100

    Add Sorting to interfaces to ensure correct order

--- Managed/UnrealSharpPrograms/UnrealSharpWeaver/TypeProcessors/UnrealInterfaceProcessor.cs
+++ Managed/UnrealSharpPrograms/UnrealSharpWeaver/TypeProcessors/UnrealInterfaceProcessor.cs
@@ -2,6 +2,7 @@
 using Mono.Cecil.Cil;
 using UnrealSharpWeaver.MetaData;
 using UnrealSharpWeaver.Utilities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace UnrealSharpWeaver.TypeProcessors;
 
@@ -10,11 +11,57 @@ public static class UnrealInterfaceProcessor
     public static void ProcessInterfaces(List<TypeDefinition> interfaces, ApiMetaData assemblyMetadata)
     {
         assemblyMetadata.InterfacesMetaData.Capacity = interfaces.Count;
-        
+
+        interfaces.Sort((a, b) =>
+        {
+            InterfaceMetaData aMetadata = new InterfaceMetaData(a);
+            InterfaceMetaData bMetadata = new InterfaceMetaData(b);
+
+            foreach (var Function in aMetadata.Functions)
+            {
+                if(Function.ReturnValue != null && Function.ReturnValue.PropertyDataType.CSharpType.FullName.Contains(bMetadata.TypeRef.FullName))
+                {
+                    return 1;
+                }
+
+                foreach (var Field in Function.Parameters)
+                {
+                    if (Field.PropertyDataType.CSharpType.FullName.Contains(bMetadata.TypeRef.FullName))
+                    {
+                        return 1;
+                    }
+                }
+            }
+
+            foreach (var Function in bMetadata.Functions)
+            {
+
+                if (Function.ReturnValue != null && Function.ReturnValue.PropertyDataType.CSharpType.FullName.Contains(aMetadata.TypeRef.FullName))
+                {
+                    return -1;
+                }
+
+                foreach (var Field in Function.Parameters)
+                {
+                    if (Field.PropertyDataType.CSharpType.FullName.Contains(aMetadata.TypeRef.FullName))
+                    {
+                        return -1;
+                    }
+                }
+            }
+
+            return 0;
+        });
+
         for (var i = 0; i < interfaces.Count; ++i)
         {
+
+
             TypeDefinition interfaceType = interfaces[i];
-            assemblyMetadata.InterfacesMetaData.Add(new InterfaceMetaData(interfaceType));
+            var a = new InterfaceMetaData(interfaceType);
+            File.AppendAllText("log.txt", a.TypeRef.FullName + Environment.NewLine);
+
+            assemblyMetadata.InterfacesMetaData.Add(a);
             
             CreateInterfaceMarshaller(interfaceType);
         }
